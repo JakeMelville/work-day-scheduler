f310aaf05081d5fed325989204d6642bcb56f969
Jake Melville <jakemelville@ymail.com>
2021-02-11 17:33:27 -0500

    set up my html. Added individual div classes forall of the time slots. Set up my current date using moments.

---
+++ assets/js/script.js
@@ -0,0 +1,81 @@
+console.log("yo");
+
+var date = document.getElementById("#current-date")
+
+function liveTime () {
+    var currentDay = moment().format("dddd, MMMM Do, YYYY");
+    $("#current-date").text(currentDay);
+}
+liveTime();
+
+console.log(currentDay);
+
+
+    //check for button clicks inside an on-click
+    
+    $(".saveBtn").on("click", function () {
+        //grab my values for time and text
+    // var time = 
+    // var text =
+    
+        //save them to local storage
+    
+    localStorage.setItem(time, text) //pass in the 2 variables for time and text
+    });
+        
+    
+    //write a function to update the hours in the time blocks 
+    //grey was passed, red is current, green is future 
+    
+    //using moment we need to get the current time 
+    
+    //loop over the timeblocks- **hint** look up .each 
+    
+    
+    //check to see if we have moved past the current time
+    //if 
+    //addClass- .past       addClass("past")<-- how it should look
+    
+    
+    
+    //check to see if it is the current time
+    //else if 
+    //removeClass ,past
+    //addClass .present
+    
+    
+    
+    //check to see if it time is future 
+    //esle
+    //removeClass .past
+    //removeClass .present
+    //addClass .future
+    
+    
+    
+    
+    //load any saved data from local storage 
+    $("#hour-9 .task").val(localStorage.getItem("hour-9"))
+    
+    
+    
+    //display current day on the page- using moment
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
